4cb54bcf87dd1a9b3c5ee5ac965539f3207f31d2
Mir Mirani <mir.mirani@outlook.com>
2020-08-24 13:04:09 +0100

    update vue

---
+++ Archives/app.js
@@ -0,0 +1,10 @@
+const heading = document.querySelector('h1');
+const input = document.querySelector('input')
+
+// listen to the input box for typing
+input.addEventListener('keyup', (e) =>
+{
+    const text = e.target.value;
+    heading.innerText = text;
+}
+)
